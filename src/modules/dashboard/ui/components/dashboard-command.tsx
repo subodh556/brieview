4b224f809de35a896921d4ea62cc67fa9c2a8e1f
Subodh <sohonisubodh@gmail.com>
2025-06-15 14:09:06 +0530

    responsive dialog

--- src/modules/dashboard/ui/components/dashboard-command.tsx
+++ src/modules/dashboard/ui/components/dashboard-command.tsx
@@ -1,4 +1,4 @@
-import { CommandDialog, CommandInput, CommandItem, CommandList } from "@/components/ui/command"
+import { CommandResponsiveDialog, CommandInput, CommandItem, CommandList } from "@/components/ui/command"
 import { Dispatch, SetStateAction } from "react";
 
 
@@ -9,13 +9,16 @@ interface Props{
 
 export const DashboardCommand = ({open, setOpen}: Props) => {
     return (
-        <CommandDialog open={open} onOpenChange={setOpen}>
+        <CommandResponsiveDialog open={open} onOpenChange={setOpen}>
             <CommandInput placeholder="Find a meeting or agent"/>
             <CommandList>
                 <CommandItem>
                     Test
                 </CommandItem>
+                <CommandItem>
+                    Test1
+                </CommandItem>
             </CommandList>
-        </CommandDialog>
+        </CommandResponsiveDialog>
     )
 }
