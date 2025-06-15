d1f99dba13ab06bba062f2efd16b8c4f6d5853e4
Subodh <sohonisubodh@gmail.com>
2025-06-15 16:17:43 +0530

    agent form

---
+++ src/modules/agents/ui/components/new-agent-dialog.tsx
@@ -0,0 +1,19 @@
+import { ResponsiveDialog } from "@/components/responsive-dialog";
+import { AgentForm } from "./agent-form";
+
+interface NewAgentDialogProps {
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+}
+
+
+export const NewAgentDialog = ({ open, onOpenChange }: NewAgentDialogProps) => {
+    return(
+        <ResponsiveDialog title="New Agent" description="Create a new agent" open={open} onOpenchange={onOpenChange}>
+          <AgentForm 
+            onSuccess={() => {onOpenChange(false)}}
+            onCancel={() => {onOpenChange(false)}}  
+          />
+        </ResponsiveDialog>
+    )
+}
