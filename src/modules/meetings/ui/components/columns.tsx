baf2a1efd8ebd8145db23fe508cc699a6db9a2a8
Subodh <sohonisubodh@gmail.com>
2025-06-21 17:11:32 +0530

    completed state

--- src/modules/meetings/ui/components/columns.tsx
+++ src/modules/meetings/ui/components/columns.tsx
@@ -4,18 +4,11 @@ import { ColumnDef } from "@tanstack/react-table"
 import { GeneratedAvatar } from "@/components/generated-avatar"
 import { Badge } from "@/components/ui/badge"
 import { MeetingGetMany } from "../../types"
-import humanizeDuration from "humanize-duration"
 import { CircleCheckIcon, CircleXIcon, ClockArrowUpIcon, ClockFadingIcon, CornerDownRightIcon, LoaderIcon } from "lucide-react"
 import { format } from "date-fns"
-import { cn } from "@/lib/utils"
+import { cn, formatDuration } from "@/lib/utils"
 
-function formatDuration(seconds: number){
-  return humanizeDuration(seconds * 1000,{ 
-    largest: 1,
-    round: true,
-    units: ["h", "m", "s"]
-  });
-}
+
 
 const statusIconMap = {
   upcoming: ClockArrowUpIcon,
